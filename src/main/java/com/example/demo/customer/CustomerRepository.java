a350a05b0e9bd7ecc6531d7d9f8a758f6c444754
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-20 13:43:49 +0530

    Replaced @Qualifier with @Autowired.

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.customer;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,6 +13,7 @@ public class CustomerRepository implements CustomerRepo {
     @Override
     public List<Customer> getCustomers() {
         // TODO connect to real db
-        return Collections.emptyList();
+        // return Collections.emptyList();
+        return Collections.singletonList(new Customer(1L, "TODO. Implement real db."));
     }
 }
