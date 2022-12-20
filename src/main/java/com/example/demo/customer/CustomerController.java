c3e5794bfaa601817f87d32f42a48b1848cb9230
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-12-20 13:13:21 +0530

    Initial commit. Created a bean and injected to the customer controller.

---
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -0,0 +1,21 @@
+package com.example.demo.customer;
+
+import com.example.demo.DemoApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping
+    Customer getCustomer() {
+        return customerService.getCustomer();
+    }
+}
